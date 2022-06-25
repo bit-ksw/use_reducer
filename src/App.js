d6a0216c1243456b1737bea8a080ea651135ed67
kimsungwon4 <skim@uppgo.jp>
2022-06-25 11:57:59 +0900

    useReducer add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from 'react'
+import Test from './components/Test'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ACTION_TYPES = {
+  deposit: 'deposit',
+  withdraw: 'withdraw'
 }
 
-export default App;
+const reducer = (state, action) => {
+  console.log("11", state, action);
+  switch (action.type) {
+    case ACTION_TYPES.deposit:
+      return state + action.payload
+    case ACTION_TYPES.withdraw:
+      return state - action.payload
+    default:
+      return state
+  }
+};
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [money, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <>
+      <div>
+        <h2>useReducer 은행에 오신것을 환영합니다.</h2>
+        <p>잔고 : {money}원</p>
+        <input type="number" value={number} onChange={(e) => setNumber(Number(e.target.value))} step="1000" />
+        <button onClick={() => {
+          dispatch({
+            type: ACTION_TYPES.deposit,
+            payload: number
+          })
+        }}>예금</button>
+        <button onClick={() => {
+          dispatch({
+            type: ACTION_TYPES.withdraw,
+            payload: number
+          })
+        }}>출금</button>
+        <br />
+        <hr />
+        <br />
+        <Test />
+      </div>
+    </>
+  )
+
+}
+
+export default App
